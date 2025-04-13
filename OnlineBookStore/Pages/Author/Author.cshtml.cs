c17d7d2d88b652bb4b7a4de2bbfd79bff22268c5
Ajaya Khanal <ajayakhanal957@gmail.com>
2025-04-13 23:11:39 +0545

    Second Changes in navbar and addition of few pages

--- OnlineBookStore/Pages/Author/Author.cshtml.cs
+++ OnlineBookStore/Pages/Author/Author.cshtml.cs
@@ -5,6 +5,11 @@ namespace OnlineBookStore.Pages.Author
 {
     public class AuthorModel : PageModel
     {
+        public int AuthorId { get; set; }
+        [BindProperty]
+        public string AuthorName { get; set; }
+
+        public List<string> Authors { get; set; } = new List<string>();
         public void OnGet()
         {
         }
--- OnlineBookStore/Pages/Book/Book.cshtml.cs
+++ OnlineBookStore/Pages/Book/Book.cshtml.cs
@@ -5,6 +5,17 @@ namespace OnlineBookStore.Pages.Book
 {
     public class BookModel : PageModel
     {
+        public int BookId { get; set; }
+        [BindProperty]
+        public string BookTitle { get; set; }
+        public string Description { get; set; }
+        public decimal Rate { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+        //public string Author { get; set; }
+        public List<String> authors { get; set; } = new List<string>();
+
+        public List<BookModel> Books { get; set; } = new List<BookModel>();
         public void OnGet()
         {
         }

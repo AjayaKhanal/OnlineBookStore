b462fe72758f331407bd075a9530943583ae436e
Ajaya Khanal <ajayakhanal957@gmail.com>
2025-06-07 17:03:21 +0545

    Permission Dto Added

---
+++ OnlineBookStore/DTO/PermissionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineBookStore.DTO
+{
+    public class PermissionDto
+    {
+        public int PermissionId { get; set; }
+        public string Name { get; set; }
+        public string Module { get; set; }
+        public string Action { get; set; }
+        public string Resource { get; set; }
+        public int IsActive { get; set; }
+        public string Message { get; set; }
+        public int Code { get; set; }
+
+    }
+}
